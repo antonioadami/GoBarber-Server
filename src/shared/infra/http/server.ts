82ce0f0521fe6f6daded48c94b381d40e664d514
Antonio Adami <antonio@b2ml.com.br>
2021-01-12 15:28:04 -0300

    Arrumando pastas e imports para ddd

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,11 +5,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
-import routes from './routes';
-import uploadConfig from './config/upload';
+import '@shared/infra/typeorm';
 
-import './database';
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 const app = express();
 
