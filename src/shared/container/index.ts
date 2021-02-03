10d274393a7f2b25d04be285aff1c8b2a60d0196
Antonio Adami <antonioadamipalma@gmail.com>
2021-02-03 20:46:55 -0300

    Registrando repositório userTokens to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,7 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
     'AppointmentsRepository',
@@ -19,7 +20,7 @@ container.registerSingleton<IUsersRepository>(
     UsersRepository,
 );
 
-// container.registerSingleton<IUserTokensRepository>(
-//     'UserTokensRepository',
-//     UserTokensRepository,
-// );
+container.registerSingleton<IUserTokensRepository>(
+    'UserTokensRepository',
+    UserTokensRepository,
+);
